90aa8090c9b248f86eeff9e757d3989acfcb137e
Akshaya Khare <akshayakhare@gmail.com>
2016-05-31 09:46:54 -0400

    Successful!!! Python

--- Service-Lane/Solution.py
+++ Service-Lane/Solution.py
@@ -3,11 +3,12 @@ import sys
 n,t = raw_input().strip().split(' ')
 n,t = [int(n),int(t)]
 width = map(int,raw_input().strip().split(' '))
+
 for a0 in xrange(t):
     i,j = raw_input().strip().split(' ')
     i,j = [int(i),int(j)]
     laneStart = i
-    min = 4
+    min = 4 #Because maximum value can only be three
     while laneStart <= j:
         if width[laneStart] < min:
             min = width[laneStart]
