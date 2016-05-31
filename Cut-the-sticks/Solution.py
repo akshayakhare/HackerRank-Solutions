62a305292138c27a9c47732639fd67b93d4619ee
Akshaya Khare <akshayakhare@gmail.com>
2016-05-31 08:02:02 -0400

    Added solution

---
+++ Cut-the-sticks/Solution.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+arr = []
+arr = map(int,raw_input().strip().split(' '))
+#print len(arr)
+while len(arr)!=0:
+    min = arr[0]
+    for i in arr:
+        if min > i:
+            min = i
+    print len(arr)
+    while arr.__contains__(min):
+        arr.remove(min)
+    #print len(arr),arr
